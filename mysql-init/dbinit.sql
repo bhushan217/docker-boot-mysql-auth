f3f5b99fb08c182155e99fc68d4bec9d09606430
bhushan217 <bhushan217@github.com>
2020-12-24 03:41:04 +0530

    initial commit

---
+++ mysql-init/dbinit.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS dockerdemo;
+CREATE DATABASE dockerdemo;
+GRANT ALL PRIVILEGES ON dockerdemo.* TO 'demo'@'%' IDENTIFIED BY 'demo';
+GRANT ALL PRIVILEGES ON dockerdemo.* TO 'demo'@'localhost' IDENTIFIED BY 'demo';
+#use dockerdemo;
+;
